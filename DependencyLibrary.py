385cd75559bf9b35dfbd4061684a68c20a8ef6ed
Alexander Kozhevnikov <mentalisttraceur@gmail.com>
2021-10-19 03:00:14 +0000

    Add MANIFEST.in to include LICENSE in sdist package

--- DependencyLibrary.py
+++ DependencyLibrary.py
@@ -1,18 +1,8 @@
-# Copyright 2017-2018 Alexander Kozhevnikov <mentalisttraceur@gmail.com>
-#
-# Permission to use, copy, modify, and distribute this software for any
-# purpose with or without fee is hereby granted.
-#
-# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+# SPDX-License-Identifier: 0BSD
+# Copyright 2017 Alexander Kozhevnikov <mentalisttraceur@gmail.com>
 
 
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 
 
 def _depends_on(status_map, dependency_type, name, status):
