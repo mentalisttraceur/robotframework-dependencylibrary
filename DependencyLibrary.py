bc45aedf16b5ca99627536ddaf1f4fac62f238c3
Alexander Kozhevnikov <mentalisttraceur@gmail.com>
2022-11-05 03:40:54 +0000

    Version string v4.0.0

--- DependencyLibrary.py
+++ DependencyLibrary.py
@@ -6,7 +6,7 @@ from robot.api import SkipExecution as _SkipExecution
 
 
 __all__ = ('DependencyLibrary',)
-__version__ = '3.0.1'
+__version__ = '4.0.0'
 
 
 def _depends_on(status_map, dependency_type, name):
